6123d3cef3fefc5f7c6584f96c860fe7b5c7219f
senghorn <rithsenghorn@gmail.com>
2023-03-15 10:15:11 -0600

    Initial Commit

---
+++ RecipeFinder/RecipeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeFinderApp.swift
+//  RecipeFinder
+//
+//  Created by Seng Rith on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
