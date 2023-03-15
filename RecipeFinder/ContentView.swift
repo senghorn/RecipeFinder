407990e68313321b2b731deab97fd38a64af4430
senghorn <rithsenghorn@gmail.com>
2023-03-15 15:22:02 -0600

    Navigation set up for dessert list and details screen.

--- RecipeFinder/ContentView.swift
+++ RecipeFinder/ContentView.swift
@@ -6,16 +6,82 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
+    @State var desserts = [DessertItem]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+                ScrollView{
+                    DessertItem(strMeal: "Whatever this is is cool", strMealThumb: "https://www.themealdb.com/images/media/meals/adxcbq1619787919.jpg", idMeal: "53049")
+                }
+                .navigationTitle("Dessert Recipes").font(.largeTitle)
+                .navigationBarTitleDisplayMode(.automatic)
+                .onAppear(perform: fetchDessertsData)
         }
-        .padding()
+    }
+    
+    func fetchDessertsData() {
+        print("Fetching Data")
+        guard let url = URL(string: "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert") else { return }
+               URLSession.shared.dataTask(with: url) { data, _, error in
+                   if let error = error {
+                       print("Error fetching data: \(error.localizedDescription)")
+                       return
+                   }
+                   guard let data = data else { return }
+                   print(data)
+               }.resume()
+    }
+}
+
+struct DessertRecipeScreen: View {
+    var id: String
+ 
+    var body: some View {
+        Text("Recipe of Dessert 1 \(id)").navigationTitle("Recipe Details")
+        
+    }
+}
+
+struct DessertItem: View {
+    var strMeal: String
+    var strMealThumb: String
+    var idMeal: String
+    
+    var body: some View {
+        NavigationLink(
+            destination: DessertRecipeScreen(id: idMeal)) {
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(.white)
+                        .frame(height: 120)
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
+                    
+                    HStack {
+                        AsyncImage(url: URL(string: "\(strMealThumb)"))
+                        { image in
+                            image.resizable()
+                                .cornerRadius(CGFloat.init(floatLiteral: 12))
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        .frame(width: 100, height: 100)
+                        .padding()
+                        
+                        Spacer()
+                        
+                        Text("\(strMeal)")
+                            .font(.headline)
+                            .foregroundColor(.black)
+                            .padding(.trailing, 10)
+                    }
+                  
+                    
+                }
+            }
     }
 }
 
